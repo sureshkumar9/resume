71f39a94398d698d166cb003a04c14a3d61f45d8
Suresh Kumar <suresh@sureshr332.successivetech.com>
2022-02-28 15:37:28 +0530

    add new files

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var typed = new Typed('#typed', {
+  strings: ["Designer.", "Developer."],
+  cursorChar: "",
+  typeSpeed: 90,
+  loop: true,
+  backSpeed: 30
+});
+const header = document.querySelector("header");
+const headerHeight = parseInt(window.getComputedStyle(header).height);
+
+document.addEventListener("scroll", function() {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header_scroll");
+  } else {
+    header.classList.remove("header_scroll");
+  };
+});
+
+
+// tabs start
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+// tab end
+
+
+// top scroll start
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// top scroll end
